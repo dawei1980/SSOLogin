6943624a4101fdfe562fc1c59b08c7048d4c16f3
Jiang dawei <dawei801230@163.com>
2020-06-09 09:52:31 +0800

    Change bugs

--- src/main/java/com/sso/login/reponsitory/UserRepository.java
+++ src/main/java/com/sso/login/reponsitory/UserRepository.java
@@ -5,6 +5,8 @@ import com.sso.login.entity.SysUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import javax.transaction.Transactional;
+import java.awt.print.Pageable;
+import java.util.List;
 
 /**
  * @Author 大誌
@@ -16,4 +18,7 @@ public interface UserRepository extends JpaRepository<SysUser, Integer> {
     SysUser findByUsername(String username);
 
     SysUser findByUserId(Integer userId);
+
+    List<SysUser> findAll();
+
 }
